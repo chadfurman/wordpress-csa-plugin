d4710930b1778696dd40e00b21b76e9b9a7ed79a
Chad Furman <chad@chadfurman.com>
2022-08-02 20:03:39 -0400

    Initial commit of plugin and .idea config

---
+++ red-fire-farm-csa-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Red Fire Farm CSA Plugin
+Description: A plugin for managing the Red Fire Farm CSA
+Version: 1.0
+Author: Chad Furman
+Author URI: https://chadfurman.com
+License: MIT
+*/
+
+

a4c32850768cf547837f8bba929b69f4fecf0844
Chad Furman <chad@chadfurman.com>
2022-08-17 08:11:36 -0400

    feat: added manage csa shares page skeleton

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './pages/Layout'
 import ManagePickupLocations from './pages/ManagePickupLocations'
 import Dashboard from "./pages/Dashboard";
 import NoPage from "./pages/NoPage";
+import ManageCSAShares from "./pages/ManageCSAShares";
 
 
 const App = () => {
@@ -12,7 +13,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<Dashboard/>} />
-                    {/*<Route path={"manage-csa-shares"} element={ManageCSAShares} />*/}
+                    <Route path={"manage-csa-shares"} element={<ManageCSAShares/>} />
                     <Route path="manage-pickup-locations" element={<ManagePickupLocations/>} />
                     <Route path="*" element={<NoPage />} />
                 </Route>
---
+++ src/pages/ManageCSAShares.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ManageCSAShares = () => {
+    return (
+        <div className="manage-csa-shares">
+            <div className="management-pane">
+                <h2>Manage CSA Shares</h2>
+                <div className="add-button">
+                    <button>Add Location</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ManageCSAShares

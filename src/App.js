f4f90287a5f739905115ebc0fa9603c696bc8b32
Chad Furman <chad@chadfurman.com>
2022-08-21 20:16:19 -0400

    wip: api connection for post types

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import ManagePickupLocations from './pages/ManagePickupLocations'
 import Dashboard from "./pages/Dashboard";
 import NoPage from "./pages/NoPage";
 import ManageCSAShares from "./pages/ManageCSAShares";
+import {QueryClient, QueryClientProvider} from "react-query";
 
+const queryClient = new QueryClient()
 
 const App = () => {
     return (
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route index element={<Dashboard/>} />
-                    <Route path={"manage-csa-shares"} element={<ManageCSAShares/>} />
-                    <Route path="manage-pickup-locations" element={<ManagePickupLocations/>} />
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
-        </HashRouter>
+        <QueryClientProvider client={queryClient}>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Layout/>}>
+                        <Route index element={<Dashboard/>} />
+                        <Route path={"manage-csa-shares"} element={<ManageCSAShares/>} />
+                        <Route path="manage-pickup-locations" element={<ManagePickupLocations/>} />
+                        <Route path="*" element={<NoPage />} />
+                    </Route>
+                </Routes>
+            </HashRouter>
+        </QueryClientProvider>
     );
 }
 
--- src/pages/ManageCSAShares.js
+++ src/pages/ManageCSAShares.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import {useQuery} from "react-query";
 
 const ManageCSAShares = () => {
+    const { isLoading, error, data } = useQuery('repoData', () =>
+        fetch('http://localhost/wordpress/?rest_route=/wp/v2/posts').then(res =>
+            res.json()
+        )
+    )
+    let posts
+    if (isLoading) {
+        posts = []
+        console.log("loading...")
+    } else {
+        posts = data.map(p => `<div>${p.title.rendered}</div>`)
+    }
     return (
         <div className="manage-csa-shares">
             <div className="management-pane">
@@ -8,6 +21,7 @@ const ManageCSAShares = () => {
                 <div className="add-button">
                     <button>Add Location</button>
                 </div>
+                {posts ? posts : "loading"}
             </div>
         </div>
     )

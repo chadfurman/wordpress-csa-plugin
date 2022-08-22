6648238a1da2ee390302bdb5659201dc37210fe1
Chad Furman <chad@chadfurman.com>
2022-08-22 06:43:51 -0400

    feat: added post types

--- red-fire-farm-csa-plugin.php
+++ red-fire-farm-csa-plugin.php
@@ -46,3 +46,28 @@ function rffcsa_admin_enqueue_scripts() {
     wp_enqueue_style( 'rffcsa-style', plugin_dir_url( __FILE__ ) . 'build/index.css' );
     wp_enqueue_script( 'rffcsa-script', plugin_dir_url( __FILE__ ) . 'build/index.js', array( 'wp-element' ), '1.0.0', true );
 }
+
+add_action('init', 'rffcsa_register_post_types');
+function rffcsa_register_post_types() {
+    register_post_type(
+        'pickup_location',
+        array(
+            'labels' => array(
+                'name'          => __('CSA Pickup Locations', 'textdomain'),
+                'singular_name' => __('CSA Pickup Location', 'textdomain'),
+            ),
+            'description' => 'A CSA Pickup location.  Examples include Granby Farm Store, Northampton Farmers\' Market, etc.  Not all locations are available at all times.'
+        )
+    );
+
+    register_post_type(
+        'share_option',
+        array(
+            'labels' => array(
+                'name'          => __('CSA Share Options', 'textdomain'),
+                'singular_name' => __('CSA Share Option', 'textdomain'),
+            ),
+            'description' => 'A CSA Share Option.  Examples include Regular, Small, Fruit, Mushroom, etc.'
+        )
+    );
+}

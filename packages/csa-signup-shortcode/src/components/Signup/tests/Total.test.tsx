b117cfda3c67ec57b3a2509d92b30bcf113bf6b9
Chad Furman <chad@chadfurman.com>
2022-10-10 18:55:55 -0400

    wip: adding placeholder tests for total

---
+++ packages/csa-signup-shortcode/src/components/Signup/tests/Total.test.tsx
@@ -0,0 +1,29 @@
+describe("Total", () => {
+    it("Displays the subtotal", () => {
+        expect(false)
+    })
+    it("Displays the delivery fee, if present", () => {
+        expect(false)
+    })
+    it("Does not display the delivery fee, if not present", () => {
+        expect(false)
+    })
+    it("Displays the boxing fee, if present", () => {
+        expect(false)
+    })
+    it("Does not display the boxing fee, if not present", () => {
+        expect(false)
+    })
+    it("Displays the total without boxing or delivery fee", () => {
+        expect(false)
+    })
+    it("Displays the total with boxing fee", () => {
+        expect(false)
+    })
+    it("Displays the total with delivery fee", () => {
+        expect(false)
+    })
+    it("Displays the total with boxing and delivery fee", () => {
+        expect(false)
+    })
+})

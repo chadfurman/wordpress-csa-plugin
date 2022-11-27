147a2b085f589dbc6e4cd99303e5432817fd1826
Chad Furman <chad@chadfurman.com>
2022-11-27 17:29:18 -0500

    chore: adding / updating types

--- packages/csa-signup-shortcode/src/types.d.ts
+++ packages/csa-signup-shortcode/src/types.d.ts
@@ -4,6 +4,7 @@ export type Region = {
     id: RegionId,
     label: RegionLabel,
 }
+export type SelectedRegion = Region
 export type Regions = Record<RegionId, Region>;
 export type SeasonId = string
 export type SeasonLabel = string
@@ -12,9 +13,11 @@ export type Season = {
     label: SeasonLabel
 }
 export type Seasons = Record<SeasonId, Season>
+export type SelectedSeasons = Seasons
 export type ShareLabel = string
 export type ShareDescription = string
 export type ShareId = string
+export type AddonShareId = ShareId
 export type Price = number
 export type Share = {
     label: ShareLabel,
@@ -24,9 +27,22 @@ export type Share = {
     regionId: RegionId,
     seasonId: SeasonId
 }
+export type AddonShare = {
+    label: ShareLabel,
+    description: ShareDescription,
+    id: AddonShareId
+    price: Price
+    regionId: RegionId,
+    seasonId: SeasonId
+}
 export type Shares = Record<ShareId, Share>
 export type SelectedShare = {
     shareId: ShareId
+    quantity: number
+}
+export type AddonShares = Record<AddonShareId, AddonShare>
+export type SelectedAddonShare = {
+    shareId: AddonShareId
     quantity: number
 }
 export type SelectedBundle = {
@@ -34,9 +50,13 @@ export type SelectedBundle = {
     bundleOptionId: BundleOptionId
 }
 export type SelectedShares = Record<ShareId, SelectedShare>
+export type SelectedAddonShares = Record<AddonShareId, AddonSelectedShare>
 export type PickupLocationId = string
 export type PickupLocationLabel = string
 export type PickupLocationDescription = string
+export type AmountToPay = Price
+export type Total = Price
+export type Subtotal = Price
 export type BoxingFee = Price
 export type DeliveryFee = Price
 export type PickupLocation = {
@@ -86,10 +106,12 @@ export type LastName = string
 export type Phone = string
 export type Email = string
 export type StreetAddress = string
+export type Address1 = StreetAddress
+export type Address2 = StreetAddress
 export type City = string
 export type State = string
 export type Zip = string
-export type CommentsType = string;
+export type Comments = string;
 export type HearAboutUsQuestionId = string;
 export type HearAboutUsQuestionLabel = string;
 export type HearAboutUsQuestion = {
@@ -107,3 +129,33 @@ export type PaymentMethod = {
 }
 export type PaymentMethods = Record<PaymentMethodId, PaymentMethod>
 export type SelectedPaymentMethod = PaymentMethod
+export type SignupFormData = {
+    firstName: FirstName
+    lastName: LastName
+    phone: Phone
+    selectedRegion: SelectedRegion,
+    selectedSeasons: SelectedSeasons,
+    selectedShares: SelectedShares,
+    selectedAddonShares: SelectedAddonShares,
+    selectedBundle: SelectedBundle,
+    selectedPickupLocation: SelectedPickupLocation,
+    selectedPaymentOption: SelectedPaymentOption,
+    selectedPaymentMethod: SelectedPaymentMethod,
+    amountToPay: AmountToPay,
+    subtotal: Subtotal,
+    total: Total,
+    boxingFee: BoxingFee,
+    deliveryFee: DeliveryFee,
+    comments: Comments,
+    selectedHearAboutUsQuestion: SelectedHearAboutUsQuestion,
+    referral: Referral,
+    firstName: FirstName,
+    lastName: LastName,
+    phone: Phone,
+    email: Email,
+    address1: Address1,
+    address2: Address2,
+    city: City,
+    state: State,
+    zip: Zip,
+}

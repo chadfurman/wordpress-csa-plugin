93d6e85b466976843abe12275acfb1eeeaf3f580
Chad Furman <chad@chadfurman.com>
2022-12-14 08:03:14 -0500

    fix: ignoring node modules after directory reorg

---
+++ api/src/controllers/test/SignupControllerTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+namespace RedFireFarm\CsaPlugin\Api\Controllers\Test;
+
+use PHPUnit\Framework\TestCase;
+
+final class SignupControllerTest extends TestCase {
+    public function testRegistersCreateHandler(): void {
+    }
+    public function testCreateHandlerAuthIsPublic(): void {
+    }
+    public function testCreateHandlerRequiresCaptcha(): void {
+    }
+    public function testRegistersListHandler(): void {
+    }
+    public function testListHandlerAuthRequiresAdmin(): void {
+    }
+}

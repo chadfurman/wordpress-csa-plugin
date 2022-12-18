52f196712cce497ce5eb4b33bbb1d9e943cfeedb
Chad Furman <chad@chadfurman.com>
2022-12-18 15:24:51 -0500

    test: first signup controller route. TBD how best to attach WP.  Considering base controller class, or possibly revisiting route registration service.  Likely going with a simple approach of base controller with central methods.  Can abstract away to service at that point if necessary later.

--- api/src/Controllers/SignupController.php
+++ api/src/Controllers/SignupController.php
@@ -7,15 +7,25 @@ use RedFireFarm\CsaPlugin\Api\Services\ConfigInterface;
 
 class SignupController extends CsaContainer
 {
-    private ConfigInterface $config;
+    private array $config;
     private string $namespace;
     private string $resource_name;
 
     public function __construct(ConfigInterface $config)
     {
-        $this->config = $config;
+        $this->config = $config->getConfig();
         $route_prefix = $this->config['route_prefix'];
         $this->namespace = $route_prefix . '/v1';
         $this->resource_name = 'signups';
+    }
+
+    public function registerRoutes(): void
+    {
+        $this->registerAuthenticatedRoute();
+    }
+
+    public function registerAuthenticatedRoute(): void
+    {
+
     }
 }
--- api/src/Controllers/Test/SignupControllerTest.php
+++ api/src/Controllers/Test/SignupControllerTest.php
@@ -4,52 +4,74 @@ namespace RedFireFarm\CsaPlugin\Api\Controllers\Test;
 
 use PHPUnit\Framework\TestCase;
 use RedFireFarm\CsaPlugin\Api\Controllers\SignupController;
+use RedFireFarm\CsaPlugin\Api\Services\Config;
 
 final class SignupControllerTest extends TestCase
 {
-    public function testRouteRegistrationHappensOnApiInit(): void
-    {
-        $this->assertTrue(!true);
-        // mock out add_action
-//        $mock_add_action = ...;
-        $signupController = new SignupController();
+    public static $functions;
 
-        $this->assertEquals('/signup', $signupController->routeName());
+    public function setUp(): void
+    {
+//        self::$functions = $this->createPartialMock(
+//            SignupController::class, []
     }
 
+//    public function testRouteRegistrationHappensOnApiInit(): void
+//    {
+//        // mock out add_action
+////        $mock_add_action = ...;
+//        $signupController = new SignupController();
+//
+//        $this->assertEquals('/signup', $signupController->routeName());
+//    }
+//
     public function testRegistersCreateHandler(): void
     {
-        $this->assertTrue(!true);
-        $controller = new SignupController();
-        $controller->register_routes();
-
-        // assert that when we call register_routes, the handler is registered.
-
-        // Function to register our new routes from the controller.
-        function prefix_register_my_rest_routes()
-        {
-        }
-
-        add_action('rest_api_init', 'prefix_register_my_rest_routes');
+        $controller = $this->getMockBuilder(SignupController::class)
+            ->onlyMethods(['registerAuthenticatedRoute'])
+            ->setConstructorArgs([new Config()])
+            ->getMock();
+        $controller->expects($this->once())->method('registerAuthenticatedRoute');
+        $controller->registerRoutes();
     }
+//        $controller = new SignupController();
+//        $controller->registerRoutes();
+//
+//        // assert that when we call register_routes, the handler is registered.
+//
+//        // Function to register our new routes from the controller.
+//        function prefix_register_my_rest_routes()
+//        {
+//        }
+//
+//        add_action('rest_api_init', 'prefix_register_my_rest_routes');
+//    }
+//
+//    public function testCreateHandlerAuthIsPublic(): void
+//    {
+//        $this->assertTrue(!true);
+//    }
+//
+//    public function testCreateHandlerRequiresCaptcha(): void
+//    {
+//        $this->assertTrue(!true);
+//    }
+//
+//    public function testRegistersListHandler(): void
+//    {
+//        $this->assertTrue(!true);
+//    }
+//
+//    public function testListHandlerAuthRequiresAdmin(): void
+//    {
+//        $this->assertTrue(!true);
+//    }
+//
+//    public function add_action(...$params): void {
+//
+//    }
+}
 
-    public function testCreateHandlerAuthIsPublic(): void
-    {
-        $this->assertTrue(!true);
-    }
-
-    public function testCreateHandlerRequiresCaptcha(): void
-    {
-        $this->assertTrue(!true);
-    }
-
-    public function testRegistersListHandler(): void
-    {
-        $this->assertTrue(!true);
-    }
-
-    public function testListHandlerAuthRequiresAdmin(): void
-    {
-        $this->assertTrue(!true);
-    }
+function add_action(...$params)
+{
 }

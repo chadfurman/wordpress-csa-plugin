3c91213292c9e727c5d42db94db5920de3002d77
Chad Furman <chad@chadfurman.com>
2023-01-21 20:36:25 -0500

    test: base controller tests fixed/added

--- api/src/Controllers/Test/BaseControllerTest.php
+++ api/src/Controllers/Test/BaseControllerTest.php
@@ -21,13 +21,72 @@ class BaseControllerTest extends TestCase {
         $this->fail("Test not implemented");
     }
 
-    public function test_get_route()
+    public function test_get_route_plural()
     {
-        $this->fail("Test not implemented");
+        $resource_name = 'a-public-resource';
+        $route = $this->base_controller->get_route(
+            resource_name: $resource_name,
+            is_singular: BaseController::PLURAL,
+            is_uuid: BaseController::UUID
+        );
+        $this->assertEquals($route, $resource_name, 'Plural routes are the resource name, no ID.');
+    }
+
+    public function test_get_route_singular_uuid()
+    {
+        $resource_name = 'a-public-resource';
+        $route = $this->base_controller->get_route(
+            resource_name: $resource_name,
+            is_singular: BaseController::SINGULAR,
+            is_uuid: BaseController::UUID
+        );
+        $this->assertStringContainsString('(?P<id>^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$)', $route, 'UUID regex should be present.');
+    }
+
+    public function test_get_route_singular_not_uuid()
+    {
+        $resource_name = 'a-public-resource';
+        $route = $this->base_controller->get_route(
+            resource_name: $resource_name,
+            is_singular: BaseController::SINGULAR,
+            is_uuid: false
+        );
+        $this->assertStringContainsString('(?P<id>[\d]+)', $route, 'Regular ID regex should be present.');
     }
 
     public function test_register_authenticated_route()
     {
-        $this->fail("Test not implemented");
+        $resource_name = 'a-public-resource';
+        $http_verb = 'POST';
+        $route_handler = function () {};
+        $schema_handler = function () {};
+        $auth_handler = function () {};
+        $is_singular = BaseController::PLURAL;
+        $is_uuid = BaseController::UUID;
+        WP_Mock::userFunction('register_rest_route', array(
+            'times' => 1,
+            'args' => array(
+                $this->base_controller->route_prefix,
+                $this->base_controller->get_route($resource_name, $is_singular, $is_uuid),
+                array(
+                    array(
+                        'methods' => $http_verb,
+                        'callback' => $route_handler,
+                        'permission_callback' => $auth_handler,
+                    ),
+                    'schema' => $schema_handler,
+                )
+            )
+        ));
+        $this->base_controller->register_authenticated_route(
+            resource_name: $resource_name,
+            http_verb: $http_verb,
+            route_handler: $route_handler,
+            schema_handler: $schema_handler,
+            auth_handler: $auth_handler,
+            is_singular: $is_singular,
+            is_uuid: $is_uuid
+        );
+        $this->assertConditionsMet();
     }
 }
